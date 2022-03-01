cc4a3c2a6bed06c06c65b91e1d38a554dc6ff163
bsm2002 <gggshhsl@gmail.com>
2022-03-01 20:03:47 +0900

    feat : add field admin

--- snugg/apps/qna/admin.py
+++ snugg/apps/qna/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from mptt.admin import DraggableMPTTAdmin
+
+from .models import Field
+
+
+class FieldAdmin(DraggableMPTTAdmin):
+    list_display = (
+        "tree_actions",
+        "indented_title",
+    )
+
+
+admin.site.register(Field, FieldAdmin)
+
 
 # Register your models here.
