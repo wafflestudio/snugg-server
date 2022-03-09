4552b04ad655e52edeb77b7b264c3a675cc1a469
bsm2002 <gggshhsl@gmail.com>
2022-03-09 13:32:58 +0900

    fix : update models.py

--- snugg/apps/qna/models.py
+++ snugg/apps/qna/models.py
@@ -1,8 +1,10 @@
-from django.db import models
+from django.contrib.contenttypes.fields import GenericForeignKey, GenericRelation
 from django.contrib.contenttypes.models import ContentType
-from django.contrib.contenttypes.fields import GenericForeignKey
+from django.db import models
 from mptt.fields import TreeForeignKey
 from mptt.models import MPTTModel
+
+from snugg.apps.user.models import User
 
 choice_limit = models.Q(model="Post") | models.Q(model="Answer")
 
@@ -16,14 +18,15 @@ class Field(MPTTModel):
 
 class Answer(models.Model):
     post = models.ForeignKey("Post", null=True, on_delete=models.CASCADE)
-    # writer = models.ForeignKey('User', null=True, on_delete=models.SET_NULL)
+    writer = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
     content = models.TextField()
+    comments = GenericRelation("Comment", related_query_name="answer")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
 
 class Comment(models.Model):
-    # writer = models.ForeignKey('User', null=True, on_delete=models.SET_NULL)
+    writer = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
     content = models.TextField()
     content_type = models.ForeignKey(
         ContentType, limit_choices_to=choice_limit, on_delete=models.CASCADE
@@ -36,7 +39,7 @@ class Comment(models.Model):
 
 class Post(models.Model):
     field = TreeForeignKey("Field", null=True, blank=True, on_delete=models.SET_NULL)
-    # writer = models.ForeignKey('User', null=True, on_delete=models.SET_NULL)
+    writer = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
     title = models.CharField(max_length=50)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
@@ -44,6 +47,7 @@ class Post(models.Model):
     accepted_answer = models.OneToOneField(
         "Answer", null=True, on_delete=models.CASCADE, related_name="bulletin"
     )
+    comments = GenericRelation("Comment", related_query_name="post")
 
 
 class Tag(models.Model):
