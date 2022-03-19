f9b5809b5224f98459d579a90d88be4edea8997e
askrid <joonwoo3023@gmail.com>
2022-03-19 14:27:25 +0900

    feat: raw viewset and serializer for Post CRUD

---
+++ snugg/apps/qna/serializers.py
@@ -0,0 +1,36 @@
+from rest_framework import serializers
+
+from .models import Post
+
+
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = (
+            "pk",
+            "field",
+            "writer",
+            "title",
+            "content",
+            "created_at",
+            "updated_at",
+            "accepted_answer",
+        )
+        read_only_fields = ("writer", "created_at", "updated_at")
+        extra_kwargs = {"field": {"required": True}}
+
+    def validate_accepted_answer(self, value):
+        if value is not None:
+            if self.instance is None:
+                raise serializers.ValidationError("질문이 만들어지기 전에 답변을 채택할 수 없습니다.")
+
+            if value.post != self.instance:
+                raise serializers.ValidationError("이 질문에 달린 답변만 채택할 수 있습니다.")
+
+        return value
+
+    def create(self, validated_data):
+        user = self.context.get("request").user
+        validated_data["writer"] = user
+
+        return super().create(validated_data)
--- snugg/apps/qna/views.py
+++ snugg/apps/qna/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from .models import Post
+from .serializers import PostSerializer
+
+
+class PostViewSet(ModelViewSet):
+    queryset = Post.objects
+    serializer_class = PostSerializer
+
+    # TODO: object level permissions for Update, Partial Update, Destroy
+    # TODO: tagging
+    # TODO: handle serializer relations for writer, accepted answer, tags, etc. (ref: https://www.django-rest-framework.org/api-guide/relations/)
+    # TODO: query optimization
