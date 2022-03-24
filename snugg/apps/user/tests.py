7dd9d1df8626d0a2e7cca427ce1a385823833cf9
askrid <joonwoo3023@gmail.com>
2022-03-24 17:12:00 +0900

    refactor: auth API test

--- snugg/apps/user/tests.py
+++ snugg/apps/user/tests.py
@@ -1,8 +1,10 @@
 import factory
 from django.test import TestCase
+from django.urls import reverse
 from factory.django import DjangoModelFactory
 from faker import Faker
 from rest_framework import status
+from rest_framework.test import APITestCase
 
 from snugg.tokens import jwt_token_of
 
@@ -24,7 +26,22 @@ class UserFactory(DjangoModelFactory):
     is_active = True
 
 
-class SignUpTests(TestCase):
+class AuthAPITestCase(APITestCase):
+    """
+    Auth API request methods included.
+    """
+
+    def signup(self, data):
+        return self.client.post(reverse("user-account-signup"), data=data)
+
+    def signin(self, data):
+        return self.client.post(reverse("user-account-signin"), data=data)
+
+    def signout(self, data):
+        return self.client.post(reverse("user-account-signout"), data=data)
+
+
+class SignUpTests(AuthAPITestCase):
     @classmethod
     def setUpTestData(cls):
         # cls.user_data = {
@@ -41,7 +58,7 @@ class SignUpTests(TestCase):
         }
 
     def test_signup(self):
-        response = self.client.post("/auth/signup/", data=self.data)
+        response = self.signup(self.data)
         user = User.objects.first()
 
         self.assertEqual(User.objects.count(), 1)
@@ -55,36 +72,34 @@ class SignUpTests(TestCase):
 
     def test_signup_email_duplicate(self):
         self.client.post("/auth/signup/", data=self.data)
-        data = self.data.copy()
-        data.update({"username": "jason"})
-        response = self.client.post("/auth/signup/", data=data)
+        data = {**self.data, "username": fake.name()}
+        response = self.signup(data)
 
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertContains(response, "email", None, status.HTTP_400_BAD_REQUEST)
         self.assertEqual(User.objects.count(), 1)
 
     def test_signup_data_insufficient(self):
         data = self.data.copy()
         data.pop("email")
-        response = self.client.post("/auth/signup/", data=data)
+        response = self.signup(data)
 
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertContains(response, "email", None, status.HTTP_400_BAD_REQUEST)
 
         data = self.data.copy()
         data["password"] = ""
-        response = self.client.post("/auth/signup/", data=data)
+        response = self.signup(data)
 
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertContains(response, "password", None, status.HTTP_400_BAD_REQUEST)
 
     def test_signup_permissions(self):
-        data = self.data.copy()
-        data.update(
-            {
-                "is_staff": True,
-                "is_superuser": True,
-                "is_active": False,
-            }
-        )
-        response = self.client.post("/auth/signup/", data=data)
+        data = {
+            **self.data,
+            "is_staff": True,
+            "is_superuser": True,
+            "is_active": False,
+        }
+
+        response = self.signup(data)
         user = User.objects.first()
 
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
@@ -93,7 +108,7 @@ class SignUpTests(TestCase):
         self.assertEqual(user.is_active, True)
 
 
-class SigninTest(TestCase):
+class SigninTests(AuthAPITestCase):
     @classmethod
     def setUpTestData(cls):
         cls.data = {
@@ -103,81 +118,85 @@ class SigninTest(TestCase):
         cls.user = UserFactory(**cls.data)
 
     def test_signin(self):
-        response = self.client.post("/auth/signin/", data=self.data)
+        response = self.signin(self.data)
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_signin_last_login(self):
-        response = self.client.post("/auth/signin/", data=self.data)
+        response = self.signin(self.data)
         prev_login = response.data.get("user").get("last_login")
 
-        response = self.client.post("/auth/signin/", data=self.data)
+        response = self.signin(self.data)
         last_login = response.data.get("user").get("last_login")
 
         self.assertGreater(last_login, prev_login)
 
     def test_signin_email_wrong(self):
-        data = self.data.copy()
-        data.update({"email": data.get("email") + "a"})
-        response = self.client.post("/auth/signin/", data=data)
+        data = {**self.data, "email": fake.email()}
+        response = self.signin(data)
 
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
     def test_signin_password_wrong(self):
-        data = self.data.copy()
-        data.update({"password": data.get("password") + "a"})
-        response = self.client.post("/auth/signin/", data=data)
+        data = {**self.data, "password": fake.password()}
+        response = self.signin(data)
 
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
     def test_signin_data_insufficient(self):
         data = self.data.copy()
         data.pop("password")
-        response = self.client.post("/auth/signin/", data=data)
+        response = self.signin(data)
 
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertContains(response, "password", None, status.HTTP_400_BAD_REQUEST)
 
         data = self.data.copy()
         data["email"] = ""
-        response = self.client.post("/auth/signin/", data=data)
+        response = self.signin(data)
 
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertContains(response, "email", None, status.HTTP_400_BAD_REQUEST)
 
 
-class SignoutTest(TestCase):
+class SignoutTests(AuthAPITestCase):
     @classmethod
     def setUpTestData(cls):
         cls.user = UserFactory()
 
+        cls.token = jwt_token_of(cls.user)
+        cls.data = {"refresh_token": cls.token.get("refresh")}
+
     def setUp(self):
-        token = jwt_token_of(self.user)
-        self.data = {"refresh_token": token.get("refresh")}
-        self.header = {"HTTP_AUTHORIZATION": f"Bearer {token.get('access')}"}
+        self.client.credentials(HTTP_AUTHORIZATION=f"Bearer {self.token.get('access')}")
 
     def test_signout(self):
-        response = self.client.post("/auth/signout/", data=self.data, **self.header)
+        response = self.signout(self.data)
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-        response = self.client.post("/auth/signout/", data=self.data, **self.header)
+        response = self.signout(self.data)
 
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
     def test_signout_refresh_token_wrong(self):
         data = self.data.copy()
         data.update({"refresh_token": fake.password()})
-        response = self.client.post("/auth/signout/", data=data, **self.header)
+        response = self.signout(data)
 
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertContains(
+            response, "refresh_token", None, status.HTTP_400_BAD_REQUEST
+        )
 
     def test_signout_refresh_token_missing(self):
         data = self.data.copy()
         data["refresh_token"] = ""
-        response = self.client.post("/auth/signout/", data=data, **self.header)
+        response = self.signout(data)
 
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertContains(
+            response, "refresh_token", None, status.HTTP_400_BAD_REQUEST
+        )
 
     def test_signout_access_token_missing(self):
-        response = self.client.post("/auth/signout/", data=self.data)
+        self.client.credentials()
+        response = self.signout(self.data)
 
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
