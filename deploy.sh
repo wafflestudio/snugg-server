243bf72108094ec24bc8e8f4b686ac17e693dd6d
bsm2002 <gggshhsl@gmail.com>
2022-03-28 23:52:49 +0900

    fix: update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,13 +4,5 @@ cd /home/ec2-user/snugg-server
 git checkout develop --quiet
 git pull origin develop
 
-source venv/bin/activate
-pip3 install -r requirements.txt --quiet
-
-python3 manage.py migrate
-python3 manage.py check
-
-pkill -f gunicorn
-gunicorn snugg.wsgi --bind 127.0.0.1:8000 --daemon
-sudo nginx -t
-sudo service nginx restart
+docker-compose down
+docker-compose up -d
