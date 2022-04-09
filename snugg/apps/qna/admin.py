e3bbc2d6ce4f07f43e80ba5d1a98ce4e0ac24386
minkyu <whizkyu@snu.ac.kr>
2022-04-09 17:53:41 +0900

    modify field admin so that name of field is displayed at admin page

--- snugg/apps/qna/admin.py
+++ snugg/apps/qna/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils.html import format_html
 from mptt.admin import DraggableMPTTAdmin
 
 from .models import Answer, Comment, Field, Post
@@ -9,6 +10,17 @@ class FieldAdmin(DraggableMPTTAdmin):
         "tree_actions",
         "indented_title",
     )
+
+    list_display_links = ("indented_title",)
+
+    mptt_level_indent = 20
+
+    def indented_title(self, instance):
+        return format_html(
+            '<div style="text-indent:{}px">{}</div>',
+            instance._mpttfield("level") * self.mptt_level_indent,
+            instance.name,  # Or whatever you want to put here
+        )
 
 
 # Register your models here.
