5f1989c1a4c9aad785a832f0d59b631fb6ace5f1
minkyu <whizkyu@snu.ac.kr>
2022-05-04 19:50:15 +0900

    Add testcase for answer API

--- snugg/apps/qna/tests_answer.py
+++ snugg/apps/qna/tests_answer.py
@@ -1,16 +1,25 @@
-from factory.django import DjangoModelFactory
+from urllib.parse import urlencode
+
 import factory
+from django.urls import reverse
+from factory.django import DjangoModelFactory
+from rest_framework import status
+from rest_framework.test import APITestCase
+
+from .models import Answer, Post
+from .tests import FieldFactory, PostAPITestCase, PostFactory, UserFactory, fake
+
 
 class AnswerFactory(DjangoModelFactory):
     class Meta:
         model = Answer
 
-    # TODO: comments
     post = factory.SubFactory(PostFactory)
     writer = factory.SubFactory(UserFactory)
     content = factory.Faker("text")
 
-class AnswerAPITestCase(APITestCase):
+
+class AnswerAPITestCase(PostAPITestCase):
     """
     Answer CRUD API request methods included.
     """
@@ -32,3 +41,48 @@ class AnswerAPITestCase(APITestCase):
 
     def destroy_answer(self, pk):
         return self.client.delete(reverse("answer-detail", args=[pk]))
+
+
+class AnswerCreateTests(AnswerAPITestCase):
+    @classmethod
+    def setUpTestData(cls) -> None:
+        cls.user = UserFactory()
+
+    def setUp(self):
+        self.client.force_authenticate(user=self.user)
+
+        self.create_post(
+            {
+                "field": FieldFactory().name,
+                "title": fake.sentence(nb_words=4),
+                "content": fake.text(),
+                "tags": fake.words(),
+            }
+        )
+
+        self.post = Post.objects.first()
+
+        self.data = {
+            "post": self.post.pk,
+            "content": fake.text(),
+        }
+
+    def test_answer_create(self):
+        response = self.create_answer(self.data)
+        answer = Answer.objects.first()
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.data.get("writer").get("pk"), self.user.pk)
+        self.assertEqual(Post.objects.count(), 1)
+        self.assertEqual(answer.writer, self.user)
+        self.assertEqual(answer.content, self.data.get("content"))
+
+    def test_answer_create_accepted_already(self):
+        response = self.create_answer(self.data)
+
+        self.post.accepted_answer = Answer.objects.first()
+        self.post.save()
+
+        response = self.create_answer(self.data)
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
